3409fae40f8bd6641e7cb90af50933a706c599f0
kick3 <kick3@DESKTOP-HT6PHOD>
2020-12-08 17:41:33 +0900

    오늘 마지막 커밋

--- src/test/java/kr/or/member/MemberService.java
+++ src/test/java/kr/or/member/MemberService.java
@@ -3,10 +3,12 @@ package kr.or.member;
 import kr.or.member.MemberVO;
 
 public class MemberService {
-	public void printMember(MemberVO[] members) {// members는 레코드가 여러개=여러줄
+	public void printMember(MemberVO[] members) {// members는 레코드가 여러개=3개레코드가들어감
 		// 서비스 구현메서드 내용(아래) 향상된 for반복문 사용
 		int cnt = 0;
 		for (MemberVO member : members) {// members여러 레코드중 1개의 레코드를 member오브젝트객체로 대입
+			//여기서 members[0]=MemberVO member -> members[1]=MemberVO member -> members[2]
+			//MemberVO클래스형데이터타입을 member 변수로 만듬.
 			cnt = cnt + 1;
 			System.out.println(cnt + "번째 레코드는 " + member.toString());
 		}
