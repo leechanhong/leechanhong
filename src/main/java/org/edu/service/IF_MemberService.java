2803f05ad292a27654e1d5a237b270f4452c9e81
kick3 <kick3@DESKTOP-HT6PHOD>
2020-12-18 19:07:33 +0900

    20201218(금) 작업 마무리

--- src/main/java/org/edu/service/IF_MemberService.java
+++ src/main/java/org/edu/service/IF_MemberService.java
@@ -3,12 +3,13 @@ package org.edu.service;
 import java.util.List;
 
 import org.edu.vo.MemberVO;
+import org.edu.vo.PageVO;
 
 /**
  * IF_MemberService클래스는 DAO를 호출하는 역할입니다.
- * @author 김일국
+ * @author 이찬홍
  *
  */
 public interface IF_MemberService {
-	public List<MemberVO> selectMember(String search_type,String search_keyword) throws Exception;
+	public List<MemberVO> selectMember(PageVO pageVO) throws Exception;
 }
