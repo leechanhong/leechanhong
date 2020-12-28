cc561367a1220ad23b93a1249707517145bd3e90
kick3 <kick3@DESKTOP-HT6PHOD>
2020-12-28 12:02:50 +0900

    3교시 커밋

---
+++ src/main/java/org/edu/dao/IF_BoardDAO.java
@@ -0,0 +1,11 @@
+package org.edu.dao;
+
+import java.util.List;
+
+import org.edu.vo.BoardVO;
+import org.edu.vo.PageVO;
+
+public interface IF_BoardDAO {
+	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception;
+	public int countBoard(PageVO pageVO) throws Exception;
+}
