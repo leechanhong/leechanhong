692e5cbdad88dea25b5e83d8bb3287a894bb49fa
kick3 <kick3@DESKTOP-HT6PHOD>
2020-12-29 16:10:29 +0900

    첨부파일기능 빼고, 게시판 CRUD 처리.

--- src/main/java/org/edu/service/BoardServiceImpl.java
+++ src/main/java/org/edu/service/BoardServiceImpl.java
@@ -43,4 +43,22 @@ public class BoardServiceImpl implements IF_BoardService {
 		return boardDAO.readAttach(bno);
 	}
 
+	@Override
+	public void insertBoard(BoardVO boardVO) throws Exception {
+		// 게시물 등록 DAO연결(아래)
+		boardDAO.insertBoard(boardVO);
+	}
+
+	@Override
+	public void deleteBoard(Integer bno) throws Exception {
+		// 게시물 삭제 DAO연결(아래)
+		boardDAO.deleteBoard(bno);
+	}
+
+	@Override
+	public void updateBoard(BoardVO boardVO) throws Exception {
+		// 게시물 수정 DAO연결(아래)
+		boardDAO.updateBoard(boardVO);
+	}
+
 }
