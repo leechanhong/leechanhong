85ce192b53339497d0f4175f78ef76c8391e5c86
kick3 <kick3@DESKTOP-HT6PHOD>
2021-01-21 17:40:06 +0900

    8교시 커밋

--- src/main/java/org/edu/controller/NaverLoginController.java
+++ src/main/java/org/edu/controller/NaverLoginController.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import javax.servlet.http.HttpSession;
 
 import org.edu.util.NaverLoginApi;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 
@@ -26,8 +28,8 @@ public class NaverLoginController {
 	 * state: 네이버App이 생성한 토큰(네트웍에서 전송되는 자료단위-인증정보)의 상태
 	 */
 	private final static String CLIENT_ID = "Spc6LMhfUJBFB4QJ3vvJ";
-	private final static String CLIENT_SECRET = "HqJYUv7DLr";
-	private final static String REDIRECT_URI = "http://127.0.0.1:8080/login";
+	private final static String CLIENT_SECRET = "--------";
+	private final static String REDIRECT_URI = "http://127.0.0.1:8080/login_callback";
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL - 사용자이름+사용자이메일 */
 	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
